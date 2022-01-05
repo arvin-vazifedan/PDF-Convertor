338e40824f1111a781736c8d97e99db987e275fa
arvin <arvinvazife@protonmail.com>
2022-01-05 19:34:19 +0330

    added new LookAndFeel

--- src/main/java/Frame.java
+++ src/main/java/Frame.java
@@ -1,6 +1,8 @@
+import com.formdev.flatlaf.FlatLightLaf;
+import com.formdev.flatlaf.intellijthemes.FlatDarkPurpleIJTheme;
+
 import javax.swing.*;
 import javax.swing.filechooser.FileFilter;
-import java.awt.*;
 import java.io.File;
 
 public class Frame {
@@ -17,33 +19,36 @@ public class Frame {
     public Frame() {
 
         chooser.addActionListener(e -> {
-            if (e.getSource() == chooser) {
-                FileFilter filter = new FileFilter() {
-                    @Override
-                    public boolean accept(File f) {
-                        String name = f.getName().toLowerCase();
-                        return name.endsWith(".pdf") || f.isDirectory();
-                    }
-
-                    @Override
-                    public String getDescription() {
-                        return "PDF Files";
-                    }
-                };
-                fc.setFileFilter(filter);
-                int returnVal = fc.showOpenDialog(panel);
-                if (returnVal == JFileChooser.APPROVE_OPTION) {
-                    file = fc.getSelectedFile();
-                    String name = file.getName();
-                    if (file.getName().length() > 20)
-                        name = file.getName().substring(0, 20) + "...";
-                    chooser.setText(name);
+            FileFilter filter = new FileFilter() {
+                @Override
+                public boolean accept(File f) {
+                    String name = f.getName().toLowerCase();
+                    return name.endsWith(".pdf") || f.isDirectory();
                 }
+
+                @Override
+                public String getDescription() {
+                    return "PDF Files";
+                }
+            };
+            fc.setFileFilter(filter);
+            int returnVal = fc.showOpenDialog(panel);
+            if (returnVal == JFileChooser.APPROVE_OPTION) {
+                file = fc.getSelectedFile();
+                String name = file.getName();
+                if (file.getName().length() > 20)
+                    name = file.getName().substring(0, 20) + "...";
+                chooser.setText(name);
             }
         });
 
         convert.addActionListener(e -> {
-            if (file != null) {
+            if (file == null) {
+                JOptionPane.showMessageDialog(jFrame,
+                    "Please Choose a PDF file first.",
+                    "Alert",
+                    JOptionPane.WARNING_MESSAGE);
+            } else {
                 String format = String.valueOf(comboBox.getSelectedItem());
                 String filePath = file.getPath();
                 filePath = file.getPath().substring(0, filePath.indexOf("."));
@@ -52,7 +57,7 @@ public class Frame {
                 String msg = "The new file is saved next to the original file.";
                 switch (format) {
                     case "txt" -> {
-                        System.out.println(filePath + ".txt");
+                        util.toTEXT(file, filePath + ".txt");
                         JOptionPane.showMessageDialog(jFrame,
                             msg,
                             title,
@@ -66,7 +71,7 @@ public class Frame {
                             JOptionPane.INFORMATION_MESSAGE);
                     }
                     case "Docx" -> {
-                        System.out.println("docx");
+                        util.toDOCX(file, filePath + ".docx");
                         JOptionPane.showMessageDialog(jFrame,
                             msg,
                             title,
@@ -74,30 +79,29 @@ public class Frame {
                     }
                     default -> throw new IllegalStateException("Unexpected value: " + format);
                 }
-            } else {
-                JOptionPane.showMessageDialog(jFrame,
-                    "Please Choose a PDF file first.",
-                    "Alert",
-                    JOptionPane.WARNING_MESSAGE);
             }
-
         });
     }
 
     public static void main(String[] args) {
 
+        try {
+            UIManager.setLookAndFeel(new FlatLightLaf());
+            FlatDarkPurpleIJTheme.setup();
+        } catch (Exception ex) {
+            System.err.println("Failed to initialize LaF");
+        }
         jFrame.setContentPane(new Frame().panel);
+        jFrame.setTitle("PDF Converter");
         jFrame.setLocationRelativeTo(null);
         jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         jFrame.setSize(450, 300);
         jFrame.setResizable(false);
-        jFrame.getContentPane().setBackground(new Color(229, 211, 206, 255));
         jFrame.setVisible(true);
     }
 
     private void createUIComponents() {
         comboBox = new JComboBox<>(formats);
     }
-
 
 }
