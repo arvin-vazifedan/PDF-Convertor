3ab7937ae5c3428e940b00579720c9fb75f530d3
arvin <arvinvazife@protonmail.com>
2022-01-09 14:58:55 +0330

    added pdf to text functionality

--- src/main/java/Util.java
+++ src/main/java/Util.java
@@ -1,32 +1,38 @@
 import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
 import org.fit.pdfdom.PDFDomTree;
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.io.Writer;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class Util {
 
-
-    public void toHTML(File file, String filePath) {
-        try (Writer output = new PrintWriter(filePath, StandardCharsets.UTF_8)) {
-            PDDocument pdf = PDDocument.load(file);
-            new PDFDomTree().writeText(pdf, output);
+    public void toText(File file, String filePath) {
+        try (BufferedWriter writer = Files.newBufferedWriter(Path.of(filePath), StandardCharsets.UTF_8)) {
+            //load pdf file
+            PDDocument document = PDDocument.load(file);
+            //get text from pdf document
+            PDFTextStripper stripper = new PDFTextStripper();
+            //write given text to output file
+            writer.write(stripper.getText(document));
         } catch (IOException ex) {
             ex.printStackTrace();
         }
     }
 
-    public void toTEXT(File file, String filePath) {
-
-
+    public void toHTML(File file, String filePath) {
+        try (BufferedWriter writer = Files.newBufferedWriter(Path.of(filePath), StandardCharsets.UTF_8)) {
+            //load pdf file
+            PDDocument pdf = PDDocument.load(file);
+            //create a new pdf DOM and write to output file
+            new PDFDomTree().writeText(pdf, writer);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
-
-    public void toDOCX(File file, String filePath) {
-
-    }
-
 
 }
